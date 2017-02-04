f36fa07da1003c75cbb255382ca05190b2ce8ba2
Adarsh Srivastava <adarsh0112@gmail.com>
2017-02-04 09:30:39 +0530

    a

--- reg.php
+++ reg.php
@@ -1,13 +1,12 @@
 <?php
 $file = 'reg.txt';
 
-$row = $_POST["name"] . $_POST["email"] . $_POST["college"] . $_POST["phno"] . $_POST["username"] . "\n"; 
+$row = $_POST["name"] . "  - " . $_POST["email"] . " - " . $_POST["college"] . " - " . $_POST["phno"] . " - " . $_POST["username"] . "\n"; 
 $ret = file_put_contents($file, $row, FILE_APPEND | LOCK_EX);
 if($ret === false) {
         die('There was an error writing this file');
     }
     else {
-        echo "$ret bytes written to file";
+        echo "You have been registered. Please proceed to solving the prolems. Good Luck!";
     }
-echo "You have been alloted the id";
 ?>
